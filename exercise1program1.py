7f80c5d1331f5fd690649d0b0a1659edd85f08bc
Tristan <Tristan.gebeaux@gmail.com>
2022-08-24 16:50:25 -0400

    First commit

---
+++ exercise1program1.py
@@ -0,0 +1,11 @@
+def get2atend():
+    if(len()){
+        return "need greater then 2 chars"
+    }
+    else{
+        return
+    }
+    end;
+str = input("input string of greater then 2 char:")
+print(str)
+print(str[-1])
